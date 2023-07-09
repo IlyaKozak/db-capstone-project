4903e111567279be17d96c882d4032274530aebf
Ilya Kozak <kozak@tut.by>
2023-07-09 17:21:43 +0300

    feat: add procedures

---
+++ Exercise3-Procedures-Optimized-Queries/CancelOrder.sql
@@ -0,0 +1,15 @@
+DELIMITER //
+CREATE PROCEDURE CancelOrder(IN id INT)
+BEGIN
+IF EXISTS(SELECT 1 FROM LittleLemonDB.Orders WHERE OrderID = id)
+THEN
+DELETE FROM LittleLemonDB.OrderDeliveryStatus WHERE OrderID = id; 
+DELETE FROM LittleLemonDB.Orders WHERE OrderID = id; 
+SELECT CONCAT("Order ", id, " is cancelled.") AS Confirmation;
+ELSE
+SELECT CONCAT("Order ", id, " does not exist.") AS Response;
+END IF;
+END //
+DELIMITER ;
+
+CALL CancelOrder(1);
---
+++ Exercise3-Procedures-Optimized-Queries/GetMaxQuantity.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+CREATE PROCEDURE GetMaxQuantity()
+BEGIN
+SELECT Quantity as "MAX Quantity" FROM LittleLemonDB.Orders
+ORDER BY Quantity DESC
+LIMIT 1;
+END //
+DELIMITER ;
+
+CALL GetMaxQuantity();
